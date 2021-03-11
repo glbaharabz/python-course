f391828cc5fb4ca085c6ec7ea5ba484a6f6159b6
Gülbahar Abazaoğlu <75485506+glbaharabz@users.noreply.github.com>
2021-03-11 16:53:04 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,12 @@
+print("CV'ler")
+
+CV1={"Ayşe Yüksel": " 19 yaşında , ögrenci, Hacettepe Üniversitesi'nde sosyoloji okuyor , orta düzeyde ingilizce biliyor " }
+CV2={"Berke Vural": "25 yaşında , işsiz , Marmara Üniveritesi Gıda Mühendisliği mezunu , ileri düzey ingilizce biliyor "}
+CV3={"Sergen Atmaca": "22 yaşında , işsiz , Hacettepe Üniversitesi Matematik mezunu , orta düzey ingilizce bilyor"}
+CV4={"Hümeyra Aksoy" : "24 yaşında , ögrenci ,ODTU Bilgisayar mühendisliği mezunu , yüksek lisans yapıyor , ileri düzey ingilizce biliyor "}
+CV5={"Oya Bilir": "30 yasında, işsiz , Gazi Üniversitesi kimya öğretmenliği mezunu , orta düzey ingilizce biliyor"}
+print(CV1)
+print(CV2)
+print(CV3)
+print(CV4)
+print(CV5)
